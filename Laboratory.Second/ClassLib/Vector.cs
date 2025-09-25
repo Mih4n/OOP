7191355f0d82f2dc33bb8e5c3d5256ec94698245
mih4n <losevmisha0@gmail.com>
2025-09-25 12:31:48 +0300

    new lab

---
+++ Laboratory.Second/ClassLib/Vector.cs
@@ -0,0 +1,28 @@
+﻿namespace ClassLib;
+
+public record struct Vector(double X, double Y, double Z)
+{
+    public static Vector operator -(Vector first, Vector second) 
+    {
+        var (fx, fy, fz) = first;
+        var (sx, sy, sz) = second;
+
+        return new Vector(fx - sx, fy - sy, fz - sy);
+    }
+
+    public static Vector operator +(Vector first, Vector second) 
+    {
+        var (fx, fy, fz) = first;
+        var (sx, sy, sz) = second;
+
+        return new Vector(fx + sx, fy + sy, fz + sy);
+    }
+
+    public static Vector operator *(Vector first, Vector second) 
+    {
+        var (fx, fy, fz) = first;
+        var (sx, sy, sz) = second;
+
+        return new Vector(fx * sx, fy * sy, fz * sy);
+    }
+}
---
+++ Laboratory.Second/ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using ClassLib;
+
+var firstVector = ReadVector("First Vector");
+var secondVector = ReadVector("Second Vector");
+
+Console.WriteLine($"Vector subtraction: { firstVector - secondVector }");
+Console.WriteLine($"Vector addition: { firstVector + secondVector }");
+Console.WriteLine($"Vector multiplication: { firstVector * secondVector }");
+
+var stopwatch = Stopwatch.StartNew();
+        
+var type = firstVector.GetType();
+
+stopwatch.Stop();
+Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
+Console.WriteLine($"Execution time: {stopwatch.ElapsedTicks} ticks");
+Console.WriteLine($"Execution time: {stopwatch.Elapsed}");
+
+stopwatch = Stopwatch.StartNew();
+        
+Console.WriteLine("Hello world");
+
+stopwatch.Stop();
+Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
+Console.WriteLine($"Execution time: {stopwatch.ElapsedTicks} ticks");
+Console.WriteLine($"Execution time: {stopwatch.Elapsed}");
+
+
+Vector ReadVector(string name)
+{
+    Console.Write($"{name} X: ");
+    double x = double.Parse(Console.ReadLine()!);
+    Console.Write($"{name} Y: ");
+    double y = double.Parse(Console.ReadLine()!);
+    Console.Write($"{name} Z: ");
+    double z = double.Parse(Console.ReadLine()!);
+    return new Vector(x, y, z);
+}
