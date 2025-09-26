d80476319d1a92365718c2dbb2c0d1691c4d5bcb
mih4n <losevmisha0@gmail.com>
2025-09-26 09:00:01 +0300

    added comments

--- Laboratory.Second/ClassLib/Vector.cs
+++ Laboratory.Second/ClassLib/Vector.cs
@@ -1,7 +1,19 @@
 ﻿namespace ClassLib.Second;
 
+/// <summary>
+/// 3 dimensioned vector
+/// </summary>
+/// <param name="X"></param>
+/// <param name="Y"></param>
+/// <param name="Z"></param>
 public record struct Vector(double X, double Y, double Z)
 {
+    /// <summary>
+    /// vector subtraction first minus second
+    /// </summary>
+    /// <param name="first"></param>
+    /// <param name="second"></param>
+    /// <returns></returns>
     public static Vector operator -(Vector first, Vector second) 
     {
         var (fx, fy, fz) = first;
@@ -10,6 +22,12 @@ public record struct Vector(double X, double Y, double Z)
         return new Vector(fx - sx, fy - sy, fz - sz);
     }
 
+    /// <summary>
+    /// vector addition 
+    /// </summary>
+    /// <param name="first"></param>
+    /// <param name="second"></param>
+    /// <returns></returns>
     public static Vector operator +(Vector first, Vector second) 
     {
         var (fx, fy, fz) = first;
@@ -18,6 +36,12 @@ public record struct Vector(double X, double Y, double Z)
         return new Vector(fx + sx, fy + sy, fz + sz);
     }
 
+    /// <summary>
+    /// vector multiplication
+    /// </summary>
+    /// <param name="first"></param>
+    /// <param name="second"></param>
+    /// <returns></returns>
     public static Vector operator *(Vector first, Vector second) 
     {
         var (fx, fy, fz) = first;
