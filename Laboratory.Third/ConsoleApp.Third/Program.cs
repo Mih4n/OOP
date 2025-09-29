3782769efc6faf286ebf6b70aa7160baeeb1efb6
mih4n <losevmisha0@gmail.com>
2025-09-29 23:31:01 +0300

    new lab

--- Laboratory.Third/ConsoleApp.Third/Program.cs
+++ Laboratory.Third/ConsoleApp.Third/Program.cs
@@ -1,10 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
 using ClassLib.Third;
 
-Console.WriteLine("Hello, World!");
+var A = ReadArray("A");
+var B = ReadArray("B");
+var C = ReadArray("C");
 
-var array = new ArrayOne([1, 2, 3 ,4]);
+WriteArray(A, "A");
+WriteArray(B, "B");
+WriteArray(C, "C");
 
-array[1] = 1;
+Console.WriteLine($"Sum is: {SumOnlyNegative(5 * A, C)}");
+Console.WriteLine($"Sum is: {SumOnlyNegative(2 * B, -A, C * 4)}");
 
-var element = array[1];
+static ArrayOne ReadArray(string name)
+{
+    Console.WriteLine($"write down an array: {name}");
+    var input = Console.ReadLine();
+    var array = input?.Split(" ").Select(int.Parse);
+
+    return new ArrayOne(array?.ToArray() ?? []);
+}
+
+static void WriteArray(ArrayOne array, string name)
+{
+    var stringBuilder = new StringBuilder();
+    stringBuilder.Append("[ ");
+    foreach (var element in array)
+        stringBuilder.AppendFormat("{0}, ", element);
+    stringBuilder.Append("]");
+
+    Console.WriteLine(stringBuilder.ToString());
+}
+
+static int SumOnlyNegative(params ArrayOne[] arrays)
+{
+    return arrays
+        .Sum(a => a.Where(e => e < 0).Sum());
+}
