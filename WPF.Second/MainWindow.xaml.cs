238a99debc65f27e05b16d4d31fa1f68bb4d36e8
Mih4n <losevmisha0@gmail.com>
2025-12-11 17:44:27 +0300

    lab 9

---
+++ WPF.Second/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Exceptions;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace WPF.Second
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            ClassLib.Second.Vector firstVector;
+            ClassLib.Second.Vector secondVector;
+            try
+            {
+                firstVector = ReadVector(VectorA_X.Text, VectorA_Y.Text, VectorA_Z.Text);
+                secondVector = ReadVector(VectorB_X.Text, VectorB_Y.Text, VectorB_Z.Text);
+            }
+            catch (VectorParametersException ex)
+            {
+                MessageBox.Show($"Ошибка ввода. Все координаты должны быть действительными числами.", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            catch
+            {
+                MessageBox.Show($"Что то пошло не так", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            ClassLib.Second.Vector subtractionResult = firstVector - secondVector;
+            ClassLib.Second.Vector additionResult = firstVector + secondVector;
+            ClassLib.Second.Vector multiplicationResult = firstVector * secondVector;
+
+            Result_Subtraction.Text = $"Вычитание (A - B): {subtractionResult}";
+            Result_Addition.Text = $"Сложение (A + B): {additionResult}";
+
+            Result_Multiplication.Text = $"Умножение (A * B ): {multiplicationResult:F2}";
+        }
+
+        private ClassLib.Second.Vector ReadVector(string xText, string yText, string zText)
+        {
+            if (double.TryParse(xText, out double x) &&
+                double.TryParse(yText, out double y) &&
+                double.TryParse(zText, out double z))
+            {
+                var vector = new ClassLib.Second.Vector(x, y, z);
+                return vector;
+            }
+                
+            throw new VectorParametersException();
+        }
+    }
+}
